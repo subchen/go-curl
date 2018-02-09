316aeb4361ffa96408ee5fcbfc37cdde3ac85e0c
Guoqiang Chen <subchen@gmail.com>
2018-02-09 21:36:57 +0800

    Update cookies.go

--- cookies.go
+++ cookies.go
@@ -3,6 +3,8 @@ package request
 import (
 	"net"
 	"net/http"
+	
+	"golang.org/x/net/publicsuffix"
 )
 
 func (r *Request) applyCookies() {
@@ -10,11 +12,21 @@ func (r *Request) applyCookies() {
 		return
 	}
 
-	jar := r.Client.Jar
+	jar := cookieJar(r.Client)
 	u, _ := URL.parse(r.URL)
 	cookies := jar.Cookies(u)
 	for k, v := range r.Cookies {
 		cookies = append(cookies, &http.Cookie{Name: k, Value: v})
 	}
 	jar.SetCookies(u, cookies)
+}
+
+func cookieJar(c *http.Client) {
+	if c.Jar == nil {
+		options := cookiejar.Options{
+			PublicSuffixList: publicsuffix.List,
+		}
+		c.Jar, _ = cookiejar.New(&options)
+	}
+	return c.Jar
 }
