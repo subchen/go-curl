8e5d91308f471e471ab07f7a8c3cbffa1a631faf
Guoqiang Chen <subchen@gmail.com>
2018-02-09 21:41:08 +0800

    Create headers.go

---
+++ headers.go
@@ -0,0 +1,29 @@
+package requests
+
+import (
+	"net/http"
+)
+
+var DefaultUserAgent = "subchen/go-request:v0.1.0"
+
+var DefaultHeaders = map[string]string{
+	"Connection":      "keep-alive",
+	"Accept-Encoding": "gzip, deflate",
+	"Accept":          "*/*",
+	"User-Agent":      DefaultUserAgent,
+}
+
+
+func (r *Request) applyHeaders(req *http.Request) {
+	// apply custom Headers
+	for k, v := range r.Headers {
+		req.Header.Set(k, v)
+	}
+
+	// apply default headers
+	for k, v := range DefaultHeaders {
+		if _, ok := r.Headers[k]; !ok {
+			req.Header.Set(k, v)
+		}
+	}
+}
