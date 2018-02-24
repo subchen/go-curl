872997bf281f26ee1cd04a33d73bf363014d233c
Guoqiang Chen <subchen@gmail.com>
2018-02-24 17:32:17 +0800

    Create redirect.go

---
+++ redirect.go
@@ -0,0 +1,43 @@
+package curl
+
+import (
+	"net/http"
+	"errors"
+)
+
+// DefaultRedirectLimit define max redirect counts
+var DefaultRedirectLimit = 10
+
+// ErrMaxRedirect when redirect times great than DefaultRedirectLimit will return this error
+var ErrMaxRedirect = errors.New("Exceeded max redirects")
+
+func defaultCheckRedirect(req *http.Request, via []*http.Request) error {
+	if len(via) > DefaultRedirectLimit {
+		return ErrMaxRedirect
+	}
+	if len(via) == 0 {
+		return nil
+	}
+	// Redirect requests with the first Header
+	for key, val := range via[0].Header {
+		// Don't copy Referer Header
+		if key != "Referer" {
+			req.Header[key] = val
+		}
+	}
+	return nil
+}
+
+func disableCheckRedirect(req *http.Request, via []*http.Request) error {
+	return http.ErrUseLastResponse
+}
+
+func (r *Request) applyRedirect() {
+	if r.Client.CheckRedirect == nil {
+		if r.RedirectDisabled {
+			r.Client.CheckRedirect = disableCheckRedirect
+		} else {
+			r.Client.CheckRedirect = defaultCheckRedirect
+		}
+	}
+}
