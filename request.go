4cb357e53190d0524ec653c71d5211625b93b2fd
Guoqiang Chen <subchen@gmail.com>
2018-02-26 14:41:23 +0800

    Update request.go

--- request.go
+++ request.go
@@ -15,15 +15,15 @@ type Request struct {
 	Client           *http.Client
 	Method           string
 	URL              string
-	QueryString      interface{}	// *url.Values, map[string]string, map[string][]string
+	QueryString      interface{} // *url.Values, map[string]string, map[string][]string
 	Headers          map[string]string
 	Cookies          map[string]string
-	Body             interface{}	// io.Reader, string
-	Json             interface{} // any
+	Body             interface{} // io.Reader, string
+	JSON             interface{} // any
 	Form             interface{} // *url.Values, map[string]string, map[string][]string
 	Files            []File
 	Auth             interface{} // authorization(BasicAuth, TokenAuth, ...), string
-	Proxy            string // http(s)://..., sock5://...
+	ProxyURL         string      // http(s)://..., sock5://...
 	RedirectDisabled bool
 }
 
@@ -76,17 +76,119 @@ func (r *Request) newURL() string {
 	return r.URL + "?" + qs.Encode()
 }
 
-func (r *Request) Reset() {
+func (r *Request) SetQuery(qs interface{}) *Request {
+	r.QueryString = qs
+	return r
+}
+
+func (r *Request) SetBody(body interface{}) *Request {
+	r.Body = body
+	return r
+}
+
+func (r *Request) SetForm(form interface{}) *Request {
+	r.Form = form
+	return r
+}
+
+func (r *Request) SetJSON(json interface{}) *Request {
+	r.JSON = json
+	return r
+}
+
+func (r *Request) AddFile(field, filename string) *Request {
+	r.Files = append(r.Files, File{
+		Fieldname: field,
+		Filename: filename,
+	})
+	return r
+}
+
+func (r *Request) AddHeader(name, value string) *Request {
+	if r.Headers = nil {
+		r.Headers = make(map[string]string)
+	}
+	r.Headers[name] = value
+	return r
+}
+
+func (r *Request) AddCookie(name, value string) *Request {
+	if r.Cookies = nil {
+		r.Cookies = make(map[string]string)
+	}
+	r.Cookies[name] = value
+	return r
+}
+
+func (r *Request) SetBasicAuth(name, passwd string) *Request {
+	r.Auth = &BasicAuth{name, passwd}
+	return r
+}
+
+func (r *Request) SetTokenAuth(token string) *Request {
+	r.Auth = &TokenAuth{token}
+	return r
+}
+
+func (r *Request) SetProxyURL(url string) *Request {
+	r.ProxyURL = url
+	return r
+}
+
+func (r *Request) Get(url string) (*Response, error) {
+	r.Method = "GET"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Post(url string) (*Response, error) {
+	r.Method = "POST"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Put(url string) (*Response, error) {
+	r.Method = "PUT"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Patch(url string) (*Response, error) {
+	r.Method = "PATCH"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Delete(url string) (*Response, error) {
+	r.Method = "DELETE"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Head(url string) (*Response, error) {
+	r.Method = "HEAD"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Options(url string) (*Response, error) {
+	r.Method = "OPTIONS"
+	r.URL = url
+	return r.Do()
+}
+
+func (r *Request) Reset() *Request {
 	r.Method = "GET"
 	r.URL = ""
 	r.QueryString = nil
 	r.Headers = map[string]string{}
 	r.Cookies = nil
 	r.Body = nil
-	r.Json = nil
+	r.JSON = nil
 	r.Form = nil
 	r.Files = nil
 	r.Auth = nil
-	r.Proxy = ""
+	r.ProxyURL = ""
 	r.RedirectDisabled = false
+	return r
 }
