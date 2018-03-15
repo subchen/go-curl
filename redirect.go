b2f48b9cb802ef245606515a146e9d4783f9d628
Guoqiang Chen <subchen@gmail.com>
2018-03-15 11:53:42 +0800

    Update redirect.go

--- redirect.go
+++ redirect.go
@@ -11,7 +11,7 @@ var DefaultRedirectLimit = 10
 // ErrMaxRedirect when redirect times great than DefaultRedirectLimit will return this error
 var ErrMaxRedirect = errors.New("Exceeded max redirects")
 
-func defaultCheckRedirect(req *http.Request, via []*http.Request) error {
+func DefaultCheckRedirect(req *http.Request, via []*http.Request) error {
 	if len(via) > DefaultRedirectLimit {
 		return ErrMaxRedirect
 	}
@@ -28,16 +28,6 @@ func defaultCheckRedirect(req *http.Request, via []*http.Request) error {
 	return nil
 }
 
-func disableCheckRedirect(req *http.Request, via []*http.Request) error {
+func DisableCheckRedirect(req *http.Request, via []*http.Request) error {
 	return http.ErrUseLastResponse
-}
-
-func (r *Request) applyRedirect() {
-	if r.Client.CheckRedirect == nil {
-		if r.RedirectDisabled {
-			r.Client.CheckRedirect = disableCheckRedirect
-		} else {
-			r.Client.CheckRedirect = defaultCheckRedirect
-		}
-	}
 }
